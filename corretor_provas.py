eff533483098e8242d28ced8762ca304a8dd7cb4
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-06-13 23:26:24 -0300

    minor corrections

--- corretor_provas.py
+++ corretor_provas.py
@@ -9,13 +9,15 @@ import csv
 
 print("Digite o nome do arquivo do banco de dados:")
 nome_do_arquivo = input()
-dados = pd.read_csv(nome_do_arquivo, sep = ';')
+dados = pd.read_csv(nome_do_arquivo, sep = ',')
 
 saida = open('notas_'+str(nome_do_arquivo)[:-4]+'.csv', 'w')
 export = csv.writer(saida, quoting = csv.QUOTE_NONNUMERIC)
 export.writerow(['Aluno', 'Nota Final'])
 
 dados['Nota Final'] = 0
+
+print("O banco de dados tem {} colunas".format(len(dados.columns)))
 
 gabarito = ['B','D','D','C','C','B','A','D','C','D','A','C','B','B','D']
 
@@ -26,15 +28,15 @@ for row in dados.index:
     nota = 0
     print("Correção da prova de {}:".format(dados["Quem é você?"][row]))
     
-    for col in range(1, len(dados.columns)-1):
-        if dados.iloc[row, col] == gabarito[col-1]:
+    for col in range(2, len(dados.columns)-1):
+        if dados.iloc[row, col] == gabarito[col-2]:
             print("{}: Correta".format(dados.columns[col]))
             nota += 0.1
-        elif dados.iloc[row, col] != gabarito[col-1]:
+        elif dados.iloc[row, col] != gabarito[col-2]:
             print("{}: Errada".format(dados.columns[col]))
             
     print("Nota final: {}\n".format(nota))
-    dados.iloc[row,16] = nota
+    dados.iloc[row, len(dados.columns)-1] = nota
     export.writerow([dados["Quem é você?"][row], nota])
 
 print(dados[['Quem é você?','Nota Final']])
