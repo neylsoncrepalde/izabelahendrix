83150d85b9bea0645060b6f87f14da64a46cf9f5
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-06-21 21:03:33 -0300

    adaptado para avaliacao integrada OK

--- corretor_provas.py
+++ corretor_provas.py
@@ -8,12 +8,13 @@ import pandas as pd
 import csv
 
 print("Digite o nome do arquivo do banco de dados:")
-nome_do_arquivo = input()
+#nome_do_arquivo = input()
+nome_do_arquivo = "~/Documentos/Corretor testes/aval_int_20171_teste.csv"
 dados = pd.read_csv(nome_do_arquivo, sep = ',')
 
 saida = open('notas_'+str(nome_do_arquivo)[:-4]+'.csv', 'w')
 export = csv.writer(saida, quoting = csv.QUOTE_NONNUMERIC)
-export.writerow(['Aluno', 'Nota Final'])
+export.writerow(['Nome', 'Nota Final'])
 
 dados['Nota Final'] = 0
 
@@ -24,23 +25,23 @@ gabarito = ['E','C','E','C','B','B','A','A','C','E',
             'B','E','E','C','D','C','D','B','B','B',
             'A','E','B','C','C']
 
-
+total = 35
 nota = 0
-for row in dados.index:
+for row in dados.index:   #Alunos
     nota = 0
-    print("Correção da prova de {}:".format(dados["Quem é você?"][row]))
+    print("Correção da prova de {}:".format(dados["NOME"][row]))
     
-    for col in range(2, len(dados.columns)-1):
-        if dados.iloc[row, col] == gabarito[col-2]:
+    for col in range(3, len(dados.columns)-1):    #Questões
+        if dados.iloc[row, col] == gabarito[col-3]:
             print("{}: Correta".format(dados.columns[col]))
-            nota += 0.1
-        elif dados.iloc[row, col] != gabarito[col-2]:
+            nota += 1
+        elif dados.iloc[row, col] != gabarito[col-3]:
             print("{}: Errada".format(dados.columns[col]))
             
-    print("Nota final: {}\n".format(nota))
-    dados.iloc[row, len(dados.columns)-1] = nota
-    export.writerow([dados["Quem é você?"][row], nota])
+    print("Nota final: {}\n".format(nota/total))
+    dados.iloc[row, len(dados.columns)-1] = nota/total
+    export.writerow([dados["NOME"][row], nota])
 
-print(dados[['Quem é você?','Nota Final']])
+print(dados[['NOME','Nota Final']])
 
 saida.close()
